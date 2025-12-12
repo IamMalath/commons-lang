ddff5725ff28afb8b44222290b23494f5c9d6114
Malath Alhashem <118542256+IamMalath@users.noreply.github.com>
2025-12-12 19:26:56 +0300

    refactor

--- src/main/java/org/apache/commons/lang3/BooleanUtils.java
+++ src/main/java/org/apache/commons/lang3/BooleanUtils.java
@@ -731,98 +731,114 @@ public class BooleanUtils {
      * @param str  the String to check; upper and lower case are treated as the same
      * @return the Boolean value of the string, {@code null} if no match or {@code null} input
      */
+
+
+
+    /** REFACTORED CODE START **/
     public static Boolean toBooleanObject(final String str) {
-        // Previously used equalsIgnoreCase, which was fast for interned 'true'.
-        // Non interned 'true' matched 15 times slower.
-        //
-        // Optimization provides same performance as before for interned 'true'.
-        // Similar performance for null, 'false', and other strings not length 2/3/4.
-        // 'true'/'TRUE' match 4 times slower, 'tRUE'/'True' 7 times slower.
         if (str == TRUE) {
             return Boolean.TRUE;
         }
         if (str == null) {
             return null;
         }
-        switch (str.length()) {
-            case 1: {
-                final char ch0 = str.charAt(0);
-                if (ch0 == 'y' || ch0 == 'Y' ||
-                    ch0 == 't' || ch0 == 'T' ||
-                    ch0 == '1') {
-                    return Boolean.TRUE;
-                }
-                if (ch0 == 'n' || ch0 == 'N' ||
-                    ch0 == 'f' || ch0 == 'F' ||
-                    ch0 == '0') {
-                    return Boolean.FALSE;
-                }
-                break;
-            }
-            case 2: {
-                final char ch0 = str.charAt(0);
-                final char ch1 = str.charAt(1);
-                if ((ch0 == 'o' || ch0 == 'O') &&
-                    (ch1 == 'n' || ch1 == 'N')) {
-                    return Boolean.TRUE;
-                }
-                if ((ch0 == 'n' || ch0 == 'N') &&
-                    (ch1 == 'o' || ch1 == 'O')) {
-                    return Boolean.FALSE;
-                }
-                break;
-            }
-            case 3: {
-                final char ch0 = str.charAt(0);
-                final char ch1 = str.charAt(1);
-                final char ch2 = str.charAt(2);
-                if ((ch0 == 'y' || ch0 == 'Y') &&
-                    (ch1 == 'e' || ch1 == 'E') &&
-                    (ch2 == 's' || ch2 == 'S')) {
-                    return Boolean.TRUE;
-                }
-                if ((ch0 == 'o' || ch0 == 'O') &&
-                    (ch1 == 'f' || ch1 == 'F') &&
-                    (ch2 == 'f' || ch2 == 'F')) {
-                    return Boolean.FALSE;
-                }
-                break;
-            }
-            case 4: {
-                final char ch0 = str.charAt(0);
-                final char ch1 = str.charAt(1);
-                final char ch2 = str.charAt(2);
-                final char ch3 = str.charAt(3);
-                if ((ch0 == 't' || ch0 == 'T') &&
-                    (ch1 == 'r' || ch1 == 'R') &&
-                    (ch2 == 'u' || ch2 == 'U') &&
-                    (ch3 == 'e' || ch3 == 'E')) {
-                    return Boolean.TRUE;
-                }
-                break;
-            }
-            case 5: {
-                final char ch0 = str.charAt(0);
-                final char ch1 = str.charAt(1);
-                final char ch2 = str.charAt(2);
-                final char ch3 = str.charAt(3);
-                final char ch4 = str.charAt(4);
-                if ((ch0 == 'f' || ch0 == 'F') &&
-                    (ch1 == 'a' || ch1 == 'A') &&
-                    (ch2 == 'l' || ch2 == 'L') &&
-                    (ch3 == 's' || ch3 == 'S') &&
-                    (ch4 == 'e' || ch4 == 'E')) {
-                    return Boolean.FALSE;
-                }
-                break;
-            }
-        default:
-            break;
-        }
 
+        switch (str.length()) {
+            case 1:
+                return handleLengthOne(str);
+            case 2:
+                return handleLengthTwo(str);
+            case 3:
+                return handleLengthThree(str);
+            case 4:
+                return handleLengthFour(str);
+            case 5:
+                return handleLengthFive(str);
+            default:
+                return null;
+        }
+    }
+
+    private static Boolean handleLengthOne(String str) {
+        char ch0 = str.charAt(0);
+        if (ch0 == 'y' || ch0 == 'Y' ||
+                ch0 == 't' || ch0 == 'T' ||
+                ch0 == '1') {
+            return Boolean.TRUE;
+        }
+        if (ch0 == 'n' || ch0 == 'N' ||
+                ch0 == 'f' || ch0 == 'F' ||
+                ch0 == '0') {
+            return Boolean.FALSE;
+        }
         return null;
     }
 
+    private static Boolean handleLengthTwo(String str) {
+        char ch0 = str.charAt(0);
+        char ch1 = str.charAt(1);
+        if ((ch0 == 'o' || ch0 == 'O') &&
+                (ch1 == 'n' || ch1 == 'N')) {
+            return Boolean.TRUE;
+        }
+        if ((ch0 == 'n' || ch0 == 'N') &&
+                (ch1 == 'o' || ch1 == 'O')) {
+            return Boolean.FALSE;
+        }
+        return null;
+    }
+
+    private static Boolean handleLengthThree(String str) {
+        char ch0 = str.charAt(0);
+        char ch1 = str.charAt(1);
+        char ch2 = str.charAt(2);
+        if ((ch0 == 'y' || ch0 == 'Y') &&
+                (ch1 == 'e' || ch1 == 'E') &&
+                (ch2 == 's' || ch2 == 'S')) {
+            return Boolean.TRUE;
+        }
+        if ((ch0 == 'o' || ch0 == 'O') &&
+                (ch1 == 'f' || ch1 == 'F') &&
+                (ch2 == 'f' || ch2 == 'F')) {
+            return Boolean.FALSE;
+        }
+        return null;
+    }
+
+    private static Boolean handleLengthFour(String str) {
+        char ch0 = str.charAt(0);
+        char ch1 = str.charAt(1);
+        char ch2 = str.charAt(2);
+        char ch3 = str.charAt(3);
+        if ((ch0 == 't' || ch0 == 'T') &&
+                (ch1 == 'r' || ch1 == 'R') &&
+                (ch2 == 'u' || ch2 == 'U') &&
+                (ch3 == 'e' || ch3 == 'E')) {
+            return Boolean.TRUE;
+        }
+        return null;
+    }
+
+    private static Boolean handleLengthFive(String str) {
+        char ch0 = str.charAt(0);
+        char ch1 = str.charAt(1);
+        char ch2 = str.charAt(2);
+        char ch3 = str.charAt(3);
+        char ch4 = str.charAt(4);
+        if ((ch0 == 'f' || ch0 == 'F') &&
+                (ch1 == 'a' || ch1 == 'A') &&
+                (ch2 == 'l' || ch2 == 'L') &&
+                (ch3 == 's' || ch3 == 'S') &&
+                (ch4 == 'e' || ch4 == 'E')) {
+            return Boolean.FALSE;
+        }
+        return null;
+    }
+
+    /** REFACTORED CODE END **/
+
+
+    
     /**
      * Converts a String to a Boolean throwing an exception if no match.
      *
